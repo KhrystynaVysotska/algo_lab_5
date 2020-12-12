1c904155604c4ce47ba6122824228a5f14a6f723
Khrystyna <khrystynavysotska@gmail.com>
2020-12-12 14:31:48 +0200

    Added same tests for alternative solution of naive algorithm

---
+++ test/brute_force_with_one_loop_test.py
@@ -0,0 +1,50 @@
+import unittest
+from alternative_solutions.substring_search_with_one_loop import find_start_and_end_of
+
+
+class BruteForceAlgorithmOneLoopTest(unittest.TestCase):
+    def test_substring_not_found(self):
+        input_string = "aabaabaabaabaabaabaab"
+        input_substring = "aaab"
+        substring_borders = find_start_and_end_of(input_string, input_substring)
+        self.assertEqual([], substring_borders)
+
+    def test_substring_one_entry(self):
+        input_string = "aaaaaaaaaawkjwkawkjlwjalawklaa"
+        input_substring = "wjal"
+        substring_borders = find_start_and_end_of(input_string, input_substring)
+        self.assertEqual([(20, 23)], substring_borders)
+
+    def test_substring_many_sequential_entries(self):
+        input_string = "aaaaaa"
+        input_substring = "aaa"
+        substring_borders = find_start_and_end_of(input_string, input_substring)
+        self.assertEqual([(0, 2), (1, 3), (2, 4), (3, 5)], substring_borders)
+
+    def test_substring_many_entries(self):
+        input_string = "asaskdasaasjdasaasacscaaasscaaasslassaccaaas"
+        input_substring = "aas"
+        substring_borders = find_start_and_end_of(input_string, input_substring)
+        self.assertEqual([(8, 10), (15, 17), (23, 25), (29, 31), (41, 43)], substring_borders)
+
+    def test_substring_in_sentence(self):
+        input_string = "The ants enjoyed the barbecue more than the family."
+        input_substring = "the"
+        substring_borders = find_start_and_end_of(input_string, input_substring)
+        self.assertEqual([(17, 19), (40, 42)], substring_borders)
+
+    def test_empty_substring(self):
+        input_string = "abcd"
+        input_substring = ""
+        substring_borders = find_start_and_end_of(input_string, input_substring)
+        self.assertEqual([], substring_borders)
+
+    def test_long_substring(self):
+        input_string = "abcd"
+        input_substring = "abc"
+        substring_borders = find_start_and_end_of(input_string, input_substring)
+        self.assertEqual([(0, 2)], substring_borders)
+
+
+if __name__ == '__main__':
+    unittest.main()
