b0d22fcd959fe778233821dc9be7b647623fda84
Khrystyna <khrystynavysotska@gmail.com>
2020-12-12 11:32:16 +0200

    Removed methods aimed to work with files

--- substring_search.py
+++ substring_search.py
@@ -1,17 +1,5 @@
 from typing import List
-
-INPUT_FILE_NAME = "strings.in"
-OUTPUT_FILE_NAME = "strings.out"
-
-
-def read_input_data() -> List:
-    with open(INPUT_FILE_NAME, "r") as input_file:
-        return input_file.readlines()
-
-
-def write_output_data(substring_borders: List) -> None:
-    with open(OUTPUT_FILE_NAME, "w") as output_file:
-        output_file.write(str(substring_borders))
+from utils.file_utils import read_input_data, write_output_data
 
 
 def find_start_and_end_of(string: str, substring: str) -> List:
